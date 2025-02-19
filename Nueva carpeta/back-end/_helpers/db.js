99a01db0e1a89902e1cb32b409a0b7d1a360b5e7
darwingdiazdev <darwingadiaz@gmail.com>
2025-02-19 19:53:17 -0400

    Delete Nueva carpeta/back-end directory

--- Nueva carpeta/back-end/_helpers/db.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    User : require('../models/user.model'),
-    Employee : require('../models/employee.model'),
-    Evaluation: require('../models/evaluation.model'),
-    FeedBack: require('../models/feedback.model')
-};
--- Nueva carpeta/back-end/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error("❌ Error:", err.message);
-
-    res.status(err.status || 500).json({
-        message: err.message || "❌ Error interno del servidor",
-    });
-};
-
-module.exports = errorHandler;
